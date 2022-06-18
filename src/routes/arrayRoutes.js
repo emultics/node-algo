4e1205de3fea55254d59c2fe5b959d170609046f
sumanmanna134 <manna.suman134@gmail.com>
2022-06-18 21:19:37 +0530

    binary search todo

--- src/routes/arrayRoutes.js
+++ src/routes/arrayRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const { getPetrolPrice } = require('../controllers/apiController');
-const { getDashedNumber } = require('../controllers/arrayController');
+const {
+  getDashedNumber,
+  binarySearch,
+} = require('../controllers/arrayController');
 const router = express.Router();
 router.route('/getDashedNumber').get(getDashedNumber);
 router.route('/getPetrolPrice').get(getPetrolPrice);
+router.route('/bsearch').post(binarySearch);
 module.exports = router;
