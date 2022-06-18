1831f848e020ec6bbdba4a780a1297b41c7297db
sumanmanna134 <manna.suman134@gmail.com>
2022-06-18 11:38:50 +0530

    add fuel api

--- src/routes/arrayRoutes.js
+++ src/routes/arrayRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const { getPetrolPrice } = require('../controllers/apiController');
 const { getDashedNumber } = require('../controllers/arrayController');
 const router = express.Router();
 router.route('/getDashedNumber').get(getDashedNumber);
-
+router.route('/getPetrolPrice').get(getPetrolPrice);
 module.exports = router;
